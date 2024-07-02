2fab165383271e74d7aca61eb8a0bad2498f1bb6
Kris Lyon <kris.lyon@gmail.com>
2024-07-02 11:32:09 -0400

    updates.

--- Set1/s1c1.py
+++ Set1/s1c1.py
@@ -1,9 +1,21 @@
-# Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
+# Matasano Crypto Challenges
+# Set 1, Challenge 1 - Convert hex to base64
 #
-# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
-
 import base64
+
 hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 byte_string = bytes.fromhex(hex_string)
-base64EncodedStr = base64.b64encode( byte_string )
-print(base64EncodedStr)
+base64EncodedStr = base64.b64encode( byte_string ).decode('utf-8')
+expected = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+# Display results.
+print('Matasano Crypto Challenges')
+print('Set 1, Challenge 1 - Convert hex to base64')
+print('------------------------------------------')
+print('Expected:   ' + expected )
+print('Calculated: ' + base64EncodedStr )
+if base64EncodedStr == expected:
+    print("Match")
+else:
+    print("No Match")
+
--- Set1/s1c2.py
+++ Set1/s1c2.py
@@ -1,7 +1,5 @@
-# S1C2.py - Fixed XOR
-# Write a function that takes two equal-length buffers and produces their XOR combination.
-#
-# If your function works properly, then when you feed it the string:
+# Matasano Crypto Challenges
+# Set 1, Challenge 2 - Fixed XOR
 #
 # 1c0111001f010100061a024b53535009181c
 # ... after hex decoding, and when XOR'd against:
@@ -15,5 +13,22 @@ def fixed_xor( buf1, buf2 ):
     return bytes([b1 ^ b2 for b1, b2 in zip(bytes.fromhex(buf1), bytes.fromhex(buf2))])
 
 if __name__ == '__main__':
-    result = fixed_xor( '1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965' )
-    print( result.hex() )
+
+    expected = '746865206b696420646f6e277420706c6179'
+    indata =  '1c0111001f010100061a024b53535009181c'
+    xorkey =  '686974207468652062756c6c277320657965'
+    result = fixed_xor( indata, xorkey )
+
+    print('Matasano Crypto Challenges')
+    print('Set 1, Challenge 2 - Fixed XOR')
+    print('------------------------------')
+
+    print('Input Data:  ' + indata )
+    print('Key Data:    ' + xorkey )
+    print('XOR Result:  ' + str(result.hex()) )
+    print('Expected:    ' + expected )
+
+    if result.hex() == expected:
+        print("Match")
+    else:
+        print("No Match")
