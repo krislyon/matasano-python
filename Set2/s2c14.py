2ed8fa9d4bdb1d3c0fd72322d612879d9bdf7b24
Kris Lyon <kris.lyon@gmail.com>
2024-07-17 23:37:46 -0400

    working, but minor bug to fix in s2c14

--- Set2/s2c14.py
+++ Set2/s2c14.py
@@ -4,9 +4,9 @@
 import sys
 import base64
 import random
-from typing import Dict
+from typing import Dict,Callable
 sys.path.append('../utils')
-from block_utils import pkcs7_pad, encrypt_aes_ecb, detect_ecb, split_blocks
+from block_utils import pkcs7_pad, encrypt_aes_ecb, detect_ecb, detect_blockcipher_metrics, split_blocks, detect_duplicate_blocks
 from text_utils import hexdump
 
 def load_base64_data( filename: str ) -> bytes:
@@ -18,7 +18,6 @@ aeskey = random.randbytes(16)
 random_prefix = random.randbytes( random.randint(0,128) )
 secretdata = load_base64_data('s2c12.dat')
 
-
 def oracle( attacker_controlled: bytes ) -> bytes:
     data = bytearray()
     data.extend( random_prefix )
@@ -28,72 +27,56 @@ def oracle( attacker_controlled: bytes ) -> bytes:
     ct = encrypt_aes_ecb( data, aeskey )
     return ct
 
-def detect_blocksize():
-    # find first ciphertext blocksize increase (full padding)
-    ctlength = len( oracle( bytes() ) )
-    prefix = ''
-    ctlength1 = ctlength
-    while(  ctlength1 == ctlength ):
-        prefix += 'A'
-        ctlength1 = len( oracle( bytes(prefix,'utf-8') ) )
-
-    # find second ciphertext blocksize increase
-    ctlength2 = ctlength1
-    while( ctlength1 == ctlength2 ):
-        prefix += 'B'
-        ctlength2 = len( oracle( bytes(prefix,'utf-8') ) )
-
-    return ( ctlength2 - ctlength1 )
-
-def create_ecb_codebook( prefix:bytes, blocksize:int=16 ) -> Dict[str,str]:
+def create_ecb_codebook( junk_offset:bytes, junk_blocks:int, prefix:bytes, blocksize:int=16 ) -> Dict[str,str]:
     assert len(prefix) == (blocksize-1), "Creating codebook with incorrect prefix length, must be blocksize-1, got: " + str(len(prefix)) 
     ecb_codebook = {}
     #print( prefix.hex() )
     #print( len(prefix) )
 
     for i in range(256):
-        pt = bytearray(prefix)
+        pt = bytearray(junk_offset)
+        pt.extend(prefix)
         pt.append(i)
         ct = oracle( bytes(pt) )
-        block = ct[0:blocksize]
-        #print( pt.hex() )
-        #print( ct.hex() ) 
-        #print(block.hex() + " : " + bytes(pt).hex())
-        ecb_codebook[block.hex()] = bytes(pt).hex()
+        block = ct[junk_blocks*blocksize:(junk_blocks+1)*blocksize]
+        # print( pt.hex() )
+        # print( ct.hex() ) 
+        #print(block.hex() + " : " + bytes(pt[-blocksize:]).hex())
+        ecb_codebook[block.hex()] = bytes(pt[-blocksize:]).hex()
     return ecb_codebook
 
-def detect_length_random_bytes():
+def detect_length_random_bytes( blocksize:int=16 ):
     # Count the number of bytes we had to add to force a duplicate block
     # (z) - random bytes, (t) - target bytes, using blocksize = 8 for readability here.
     # case1: random bytes are not-block aligned  zzzzAAAA AAAAAAAA AAAAAAAA tttttttt
     # case2: random bytes are block-aligned      zzzzzzzz AAAAAAAA AAAAAAAA tttttttt
     # randomByteLength = (lastBlockMatchStart * blocksize) - (count - blocksize);
     for count in range(blocksize*2,blocksize*3):
-        prefix = bytes('A' * count,'utf-8')
-        ct = oracle( prefix )
-        blocks = split_blocks( ct )
-        for block1 in blocks:
-            blockcount = 0
-            for block2 in blocks:
-                if( block1 == block2 ):
-                    blockcount += 1
-            if blockcount > 1:
-                print('Count: ' + str(count))
-                return count
+        known_pt = bytes('A' * count,'utf-8')
+        ct = oracle( known_pt )
+        (dup_block_found,dup_block_num) = detect_duplicate_blocks(ct)
 
-        assert True, 'shouldnt get here'
+        if dup_block_found:
+            return (dup_block_num * blocksize) - (count-2*blocksize)   
 
-def recover_block( blocknum, recovered_data, secretdata, key ):
+    
+
+def recover_block( junk_offset:int, junk_blocks:int, blocknum:int, recovered_data:bytes, recovery_target:int=0 ):
     recovered_block = bytearray()
 
     for byte_num in range(1,blocksize+1):
 
+        if recovery_target != 0:
+            if len(recovered_data) + len(recovered_block) == recovery_target:
+                print('Recovery Target Reached, breaking and padding.')
+                break
+
         # prefix our knowndata
-        if blocknum == 0:
+        if len(recovered_data)==0:
             # Account for our translation padding in first blocks
             prefix = bytearray( 'A' * (blocksize-byte_num),'utf-8')
             prefix.extend(recovered_block)
-            codebook = create_ecb_codebook( prefix )
+            codebook = create_ecb_codebook( junk_offset, junk_blocks, prefix )
         else:
             # Need to use known data
             rdatalen = len(recovered_data)
@@ -102,24 +85,28 @@ def recover_block( blocknum, recovered_data, secretdata, key ):
             prefix.extend(recovered_block)
             # print( 'bk: ' + recovered_block.hex() )
             # print( 'px: ' + prefix.hex() )
-            codebook = create_ecb_codebook( prefix )
+            codebook = create_ecb_codebook( junk_offset, junk_blocks, prefix )
 
         # translate target byte to the end of the block
-        translation_padding = bytes('A' * (blocksize-byte_num),'utf-8')
-        ct = oracle( translation_padding )
+        data = bytearray('o' * junk_offset, 'utf-8')
+        data.extend( bytes('A' * (blocksize-byte_num),'utf-8') )
+        ct = oracle( data )
 
         # look up the plaintext for the block, recovering the byte
         block = ct[blocknum*blocksize:((blocknum+1)*blocksize)]
         print( 'ct: ' + block.hex() )
         print( 're: ' + recovered_block.hex() )
-        pt = bytes.fromhex( codebook[ block.hex() ] )
+        pt = bytes.fromhex( codebook[block.hex()] )
         print( 'pt: ' + codebook[block.hex()] )
 
         # store the recovered byte
         recovered_block.append(pt[15]) 
 
+    if recovery_target != 0 and len(recovered_block) != blocksize:
+        pad = blocksize - len(recovered_block)
+        for i in range(pad):
+            recovered_block.append(pad)
 
-    assert len(recovered_block) == 16, "Off by one."
     return bytes(recovered_block)
 
 if __name__ == '__main__':
@@ -130,26 +117,38 @@ if __name__ == '__main__':
     key = random.randbytes(16)
     secretdata = load_base64_data('s2c12.dat')
 
-    # Determine Blocksize - observe changes in ciphertext length to determine blocksize
-    blocksize = detect_blocksize()
-    print('Detected Blocksize: ' + str(blocksize))
-
+    # Calculate Message Metrics
+    (blocksize, block_count, ctlength, pad_length, msg_length) = detect_blockcipher_metrics( oracle )
+    print('Blocksize:\t\t' + str(blocksize))
+    print('Block Count:\t\t' + str(block_count))
+    print('Ciphertext Length:\t' + str(ctlength))
+    print('Padding:\t\t' + str(pad_length))
+    print('Message Length:\t\t' + str(msg_length))
 
     # Detect ECB - we know the blocksize, we can force a block repeat with the prefix
     prefix = bytes( 'A' * (blocksize*3) ,'utf-8')
     using_ecb = detect_ecb( oracle( prefix ) )
-    print('ECB Detected: ' + str(using_ecb))
-
+    print('ECB Detected:\t\t' + str(using_ecb))
+    print('')
     # Detect Random Byte Length
     rand_byte_length = detect_length_random_bytes()
     print('Random Byte Count Detected: ' + str(rand_byte_length) )
 
-    # Decrypt with Attack
-    # recovered_data = bytearray()
-    # for blocknum in range( int(len(secretdata) / blocksize) ):
-    #     result = recover_block( blocknum, bytes(recovered_data), secretdata, key)
-    #     recovered_data.extend( result )
-    #     print('Block ' + str(blocknum) + ' Complete: ' + result.hex() )
+    # Determine how many junk bytes we need to add to complete the first random blocks.
+    junk_count = 0
+    while (rand_byte_length + junk_count) % blocksize != 0:
+        junk_count += 1
+    print('Junk Byte Count: ' + str(junk_count) )
+    junk_blocks = int( (rand_byte_length + junk_count) / blocksize )
+    print('Junk Blocks (skip): ' + str(junk_blocks) )
 
-    # print( recovered_data.decode('utf-8'))
+
+    # Decrypt with Attack
+    recovered_data = bytearray()  
+    for blocknum in range( junk_blocks, block_count ):
+        result = recover_block( junk_count, junk_blocks, blocknum, bytes(recovered_data), msg_length-(rand_byte_length+junk_count))
+        recovered_data.extend( result )
+        print('Block ' + str(blocknum) + ' Complete: ' + result.hex() )
+
+    print( recovered_data.decode('utf-8'))
 
