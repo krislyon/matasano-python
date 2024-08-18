ebb63e215606cad84f666956deb8fe4ed8691432
Kris Lyon <kris.lyon@gmail.com>
2024-08-18 12:06:41 -0400

    Completed s5c34.

---
+++ Set5/s5c34-cli.py
@@ -0,0 +1,92 @@
+# Matasano Crypto Challenges
+# Set 5, Challenge 34 - Implement a MITM key-fixing attack on Diffie-Hellman with parameter injection
+#
+import sys
+sys.path.append('../utils')
+import argparse
+import dh_utils
+import sha1_utils
+import block_utils
+import random
+import requests
+import json
+
+
+SERVER_ADDRESS = 'http://127.0.0.1'
+SERVER_PORT = '5001'
+URL_KEY_EXCHG = "/key-exchange"
+URL_MESSAGE = "/secure-message"
+
+
+def derive_key( shared_secret:bytes ):
+    return sha1_utils.sha1_hash( shared_secret )[0:16]
+
+def decrypt_msg( ct:bytes, key:bytes, iv:bytes ):
+    padded = block_utils.decrypt_aes_manual_cbc( ct, key, iv )
+    pt = block_utils.pkcs7_unpad( padded )
+    return pt 
+
+def encrypt_msg( pt:bytes, key:bytes, iv:bytes ):
+    padded = block_utils.pkcs7_pad(pt)
+    ct = block_utils.encrypt_aes_manual_cbc( padded, key, iv )
+    return ct
+
+def request_key_exchange( p:bytes, g:bytes ):
+    (client_pub, client_priv) = dh_utils.dh_create_key_exchg_data( p, g )
+
+    payload = {"p": p.hex(), "g": g.hex(), "A": client_pub.hex()  }
+    request_url = SERVER_ADDRESS + ':' + SERVER_PORT + URL_KEY_EXCHG
+    response = requests.get( request_url, params=payload )
+    jdict = response.json()
+
+    session_id = bytes.fromhex( jdict['session-id'] )
+    B = bytes.fromhex( jdict['B'] )
+    iv = bytes.fromhex( jdict['session-iv'] )
+
+    shared_secret = dh_utils.dh_create_shared_secret( client_priv, B, p )
+    key = derive_key( shared_secret )
+
+    return ( session_id, key, iv )
+
+def request_secret_message( session_id:bytes, ciphertext:bytes ):
+    payload = { "session-id":session_id.hex(), "message-data":ciphertext.hex() }
+    request_url = SERVER_ADDRESS + ':' + SERVER_PORT + URL_MESSAGE
+    response = requests.get( request_url, params=payload )
+    jdict = response.json()
+    return bytes.fromhex( jdict['message-data'] )
+
+if __name__ == '__main__':
+    print('Matasano Crypto Challenges')
+    print('Set 5, Challenge 34 (Server) - MITM key-fixing attack on Diffie Hellman')
+    print('----------------------------------------------------------------------------')
+    print()
+
+    p = bytes.fromhex(dh_utils.NIST_P)
+    g = bytes.fromhex(dh_utils.NIST_G)
+
+    ( session_id, key, iv ) = request_key_exchange( p, g )
+
+    print(f'session-id:\t{session_id.hex()}')
+    print(f'key:\t\t{key.hex()}')
+    print(f'iv:\t\t{iv.hex()}')
+
+    pt = 'I get my kicks on channel six'
+    print(f'SEND({session_id.hex()}): {pt}')    
+    ct = encrypt_msg( bytes(pt,'utf-8'), key, iv )
+    resp_ct = request_secret_message( session_id, ct )
+    pt = decrypt_msg( resp_ct, key, iv )
+    print(f'RECV({session_id.hex()}): {pt.decode('utf-8')}')
+
+    pt = 'The light it burns my eyes and I feel so dirty'
+    print(f'SEND({session_id.hex()}): {pt}')    
+    ct = encrypt_msg( bytes(pt,'utf-8'), key, iv )
+    resp_ct = request_secret_message( session_id, ct )
+    pt = decrypt_msg( resp_ct, key, iv )
+    print(f'RECV({session_id.hex()}): {pt.decode('utf-8')}')
+
+    pt = 'Here comes Christ on crutches'
+    print(f'SEND({session_id.hex()}): {pt}')    
+    ct = encrypt_msg( bytes(pt,'utf-8'), key, iv )
+    resp_ct = request_secret_message( session_id, ct )
+    pt = decrypt_msg( resp_ct, key, iv )
+    print(f'RECV({session_id.hex()}): {pt.decode('utf-8')}')
---
+++ Set5/s5c34-mitm.py
@@ -0,0 +1,99 @@
+# Matasano Crypto Challenges
+# Set 5, Challenge 34 - Implement a MITM key-fixing attack on Diffie-Hellman with parameter injection
+#
+import sys
+sys.path.append('../utils')
+import argparse
+import dh_utils
+import sha1_utils
+import block_utils
+import random
+import requests
+from flask import Flask, jsonify, request
+
+parser = argparse.ArgumentParser( "Set-5, Challenge-34 MitM Proxy", description="Server for matasano challenges" )
+args = parser.parse_args()
+app = Flask(__name__)
+
+sessions = {}
+SERVER_ADDRESS = 'http://127.0.0.1'
+SERVER_PORT = '5000'
+URL_KEY_EXCHG = "/key-exchange"
+URL_MESSAGE = "/secure-message"
+
+
+
+def forwardSecureMessage(payload):
+    request_url = SERVER_ADDRESS + ':' + SERVER_PORT + URL_MESSAGE
+    response = requests.get( request_url, params=payload )
+    jdict = response.json()
+    return (jdict, response.status_code)
+
+def forwardKeyExchange(payload):
+    request_url = SERVER_ADDRESS + ':' + SERVER_PORT + URL_KEY_EXCHG
+    response = requests.get( request_url, params=payload )
+    jdict = response.json()
+    return (jdict, response.status_code)
+
+def derive_key():
+    # with the parameter injection of 'p', the value of the shared secret is reduced to 0.
+    # we need to use the appropriate byte length 192 and use this as the secret.
+    zero_val = 0
+    shared_secret = zero_val.to_bytes( 192, 'little', signed=False )
+    return sha1_utils.sha1_hash( shared_secret )[0:16]
+
+def decrypt_msg( ct:bytes, key:bytes, iv:bytes ):
+    padded = block_utils.decrypt_aes_manual_cbc( ct, key, iv )
+    pt = block_utils.pkcs7_unpad( padded )
+    return pt 
+
+
+@app.route('/key-exchange', methods=['GET'])
+def handleKeyExchange():
+    debug = False
+    dh_p = bytes.fromhex( request.args.get('p') )
+    dh_g = bytes.fromhex( request.args.get('g') )
+    dh_A = bytes.fromhex( request.args.get('A') )
+
+    # manipulate_client_message
+    client_payload = {"p": dh_p.hex(), "g": dh_g.hex(), "A": dh_p.hex()  }
+
+    (server_payload,response_code) = forwardKeyExchange( client_payload )
+    # manipulate_server_message
+    server_payload['B'] = dh_p.hex()   
+
+    sessions[ server_payload['session-id'] ] = ( derive_key(), bytes.fromhex(server_payload['session-iv']) )
+
+    return (jsonify(server_payload), response_code )
+
+
+@app.route('/secure-message', methods=['GET'])
+def handleSecureMessage():
+    session_id = request.args.get('session-id') 
+    session = sessions[session_id]
+    session_key = session[0]
+    session_iv =  session[1]
+
+    # Decrypt and print client message    
+    ct = bytes.fromhex( request.args.get('message-data') )
+    client_pt = decrypt_msg(ct, session_key, session_iv ).decode('utf-8')
+    print(f'CLI --> SERV ({session_id}):\t{client_pt}')
+
+    client_payload = { 'session-id': session_id, 'message-data': ct.hex() }
+    (server_payload, response_code) = forwardSecureMessage( client_payload )
+
+    # Decrypt and print server message
+    ct = bytes.fromhex( server_payload['message-data'] )
+    server_pt = decrypt_msg(ct, session_key, session_iv ).decode('utf-8')
+    print(f'SERV --> CLI ({session_id}):\t{server_pt}')
+
+
+    return (jsonify(server_payload), response_code )
+ 
+
+if __name__ == '__main__':
+    print('Matasano Crypto Challenges')
+    print('Set 5, Challenge 34 (Server) - MITM key-fixing attack on Diffie Hellman')
+    print('----------------------------------------------------------------------------')
+    print()
+    app.run(port=5001 )
